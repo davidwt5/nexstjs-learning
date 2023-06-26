bf0075382db2c0e66139ec9afb4b0ee12c46b712
davidwt5 <davidwtan75@gmail.com>
2023-06-27 07:04:29 +1000

    CH2: Added layout for About route

---
+++ app/about/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  <div>
+    <nav>About navbar</nav>
+    {children}
+  </div>;
+}
